2544edd613ef0ef6cb6e528b18cfc8601a39dd75
KyluAce <kylu1@op.pl>
2015-05-18 18:28:51 +0200

    actionBar

--- app/src/main/java/com/example/michal/projektwzorce/presenter/PictureEdit.java
+++ app/src/main/java/com/example/michal/projektwzorce/presenter/PictureEdit.java
@@ -3,37 +3,80 @@ package com.example.michal.projektwzorce.presenter;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.Toast;
 import android.view.MenuItem;
 
 import com.example.michal.projektwzorce.R;
 
-public class PictureEdit extends Activity {
+public class PictureEdit extends Activity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture_edit);
-    }
 
+    }
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         getMenuInflater().inflate(R.menu.menu_picture_edit, menu);
         return true;
-    }
 
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+
+            case R.id.seekbar1:
+
+                homeActivity();
+
+                return true;
+
+            case R.id.seekbar2:
+
+                homeActivity();
+
+                return true;
+
+            case R.id.przyciecie:
+
+                homeActivity();
+
+                return true;
+
+            case R.id.home:
+
+                javaActivity();
+
+                return true;
+
+            case R.id.facebook:
+
+                androidActivity();
+
+                return true;
+
+            default:
+
+                return super.onOptionsItemSelected(item);
+
         }
-
-        return super.onOptionsItemSelected(item);
+    }
+    public void homeActivity()
+    {
+        Toast.makeText(this, "Home Option Selexted", Toast.LENGTH_SHORT).show();
+    }
+    public void javaActivity()
+    {
+        Toast.makeText(this, "Java Option Selexted", Toast.LENGTH_SHORT).show();
+    }
+    public void androidActivity()
+    {
+        Toast.makeText(this, "Android Option Selexted", Toast.LENGTH_SHORT).show();
     }
 }
+
