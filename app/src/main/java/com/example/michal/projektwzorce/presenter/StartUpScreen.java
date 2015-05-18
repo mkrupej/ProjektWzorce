dc3d36c199d366b4249cb9a5d671bc631bfa3608
KyluAce <kylu1@op.pl>
2015-05-18 15:37:55 +0200

    Startup Screen

--- app/src/main/java/com/example/michal/projektwzorce/presenter/StartUpScreen.java
+++ app/src/main/java/com/example/michal/projektwzorce/presenter/StartUpScreen.java
@@ -1,9 +1,14 @@
 package com.example.michal.projektwzorce.presenter;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
 /**
  * Created by Krzysztof on 2015-05-18.
  */
-public class StartUpScreen {
-    //gagaadfdssdsdf
-    //22cc
+public class StartUpScreen extends Activity
+{
+
 }
