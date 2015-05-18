0be602d337fca9b372a59f88b81027026fae8272
KyluAce <kylu1@op.pl>
2015-05-18 16:33:34 +0200

    Startup Screen

--- app/src/main/java/com/example/michal/projektwzorce/presenter/MainActivity.java
+++ app/src/main/java/com/example/michal/projektwzorce/presenter/MainActivity.java
@@ -1,19 +1,37 @@
 package com.example.michal.projektwzorce.presenter;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Handler;
 
 import com.example.michal.projektwzorce.R;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity
+{
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, ChoiceActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 
 }
+
+
