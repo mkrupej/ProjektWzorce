b076ca1132a044416777bea51021a8ab46096be2
KyluAce <kylu1@op.pl>
2015-06-10 18:58:24 +0200

    ta

---
+++ app/src/main/java/com/example/michal/projektwzorce/controller/Stretegy/FacebookStrategy.java
@@ -0,0 +1,7 @@
+package com.example.michal.projektwzorce.controller.Stretegy;
+
+/**
+ * Created by Krzysztof on 2015-06-10.
+ */
+public class FacebookStrategy {
+}
---
+++ app/src/main/java/com/example/michal/projektwzorce/controller/Stretegy/ShareStrategy.java
@@ -0,0 +1,10 @@
+package com.example.michal.projektwzorce.controller.Stretegy;
+
+/**
+ * Created by Krzysztof on 2015-06-10.
+ */
+public interface ShareStrategy
+{
+
+
+}
